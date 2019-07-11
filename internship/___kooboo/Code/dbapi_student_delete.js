1fb42e54b1323bde1cb6c78dac3660a0b2a6c39a
Yluo <1106979076@qq.com>
2019-07-11 13:44:05 +0800

    测试4

---
+++ internship/___kooboo/Code/dbapi_student_delete.js
@@ -0,0 +1,8 @@
+if (k.request.method == 'DELETE') {
+    var id = k.request.id;
+    var i == k.database.student.get(id);
+    if(i){
+    k.database.student.delete(id);
+    var obj = {};
+    obj.code = ;}
+}
