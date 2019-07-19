00cd89d7cf008789f7962d17851fd981d8d997e8
monster <1093846898@qq.com>
2019-07-19 14:01:15 +0800

    stu apply finish

--- stu_apply.js
+++ stu_apply.js
@@ -19,18 +19,18 @@ if(k.request.method == 'POST'){
 if(k.request.method == 'PUT'){
     var apply_id = k.request.apply_id;
     var apply_status = k.request.apply_status;
-	// var back = k.session.get("key");
- //    var studentId = back.studentId;
- //    if(!studentId || studentId != stu_id){
- //    	var obj = {code:"100", msg:"登陆已过期，请重新登陆"};
- //        k.response.json(obj);
- //    }
- //    else{
+	var back = k.session.get("key");
+    var studentId = back.studentId;
+    if(!studentId || studentId != stu_id){
+    	var obj = {code:"100", msg:"登陆已过期，请重新登陆"};
+        k.response.json(obj);
+    }
+    else{
         var table = k.database.getTable("stu_apply");
         var stu_apply = table.find("_id",apply_id);
         stu_apply.apply_status = apply_status;
         table.update(stu_apply);
         var obj = {code:"200", msg:"处理成功"};
         k.response.json(obj);
-    // }
+    }
 }
