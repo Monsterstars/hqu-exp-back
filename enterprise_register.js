d54bb35d478bd0cfe16dae6dec47b904e9b4e96a
monster <1093846898@qq.com>
2019-07-19 09:15:20 +0800

    enterprise register finish

--- enterprise_register.js
+++ enterprise_register.js
@@ -1,6 +1,6 @@
 if(k.request.method == 'POST'){
     // 1、获取前端数据
-	var type = k.request.type;
+    var type = k.request.type;
     var name = k.request.name;
     var address = k.request.address;
     var email = k.request.email;
@@ -8,6 +8,7 @@ if(k.request.method == 'POST'){
     var contact_tel = k.request.contact_tel;
     var qualificate_file = k.request.qualificate_file;
     var intro = k.request.intro;
+    var password = k.request.password;
     // 2、生成当前时间
     var time = new Date();
     // 3、判断输入数据是否符合要求
@@ -26,6 +27,7 @@ if(k.request.method == 'POST'){
             qualificate_file: qualificate_file,
             intro: intro,
             register_time: time,
+            password: password,
             status: 0,
         };
         var returnid = table.add(database_obj);
@@ -38,34 +40,40 @@ if(k.request.method == 'POST'){
         k.response.json(obj);
     }
 }
-
+else{
+    var obj = {code:"100",msg:"method not found"}
+    k.response.json(obj);
+}
 
 function msg(type, name, address, email, contact_name, contact_tel, qualificate_file, intro){
-	if(!type){
-    	return "请选择类型";
+    if(!type){
+        return "请选择类型";
     }
     if(!name){
-    	return "请输入名称";
+        return "请输入名称";
     }
     if(!address){
-    	return "请输入地址";
+        return "请输入地址";
     }
     if(!email){
-    	return "请输入邮箱";
+        return "请输入邮箱";
     }
     if(!contact_name){
-    	return "请输入联系人姓名";
+        return "请输入联系人姓名";
     }
     if(!contact_tel){
-    	return "请输入联系人电话";
+        return "请输入联系人电话";
     }
     if(!qualificate_file){
-    	return "请上传资质文件";
+        return "请上传资质文件";
     }
     if(!intro){
-    	return "请输入简介";
+        return "请输入简介";
+    }
+    if(!password){
+        return "请输入密码";
     }
     else{
-    	return "注册成功";
+        return "注册成功";
     }
 }
