56f0ae68b9ebf9eb356ab53a45b7425429af7210
Yluo <1106979076@qq.com>
2019-07-24 23:32:36 +0800

    student_detail更新

--- student_detail.js
+++ student_detail.js
@@ -2,7 +2,9 @@
  * 管理员/企业查询学生信息的api
  * 
  */
-var sessionObj = {enterpriseId: 1725121008};
+var sessionObj = {
+    enterpriseId: 1725121008
+};
 k.session.set("key", sessionObj);
 //k.session.clear();
 var code = "100";
@@ -11,27 +13,30 @@ var obj = {};
 if (k.request.method == 'GET') {
     //判断是否为“GET”请求
     var back = k.session.get("key");
-    if (!back) { 
+    if (!back) {
         //判断身份是否为管理员/企业
         msg = "登陆已过期，请重新登陆";
     } else {
         var adminId = back.adminId;
         var enterpriseId = back.enterpriseId;
-        if (!(adminId||enterpriseId)) {
+        if (!(adminId || enterpriseId)) {
             //判断身份是否为学生
             msg = "登陆已过期，请重新登陆";
         } else {
             var stuId = k.request.student
             var stuTable = k.database.getTable("student");
             var student = stuTable.get(stuId);
-            student.password = "**********";
-            code = "200";
-            msg = "查询成功";
-            obj.data = student;
+            if (student) {
+                student.password = "**********";
+                code = "200";
+                msg = "查询成功";
+                obj.data = student;
+            } else {
+                msg = "未取得学生信息";
+            }
         }
     }
 }
-
 obj.code = code;
 obj.msg = msg;
 k.response.json(obj);
