50361fb358c27a67d53730eab1392480cf3ba9dd
Yluo <1106979076@qq.com>
2019-07-25 10:55:25 +0800

    student_application更新

--- stu_apply.js
+++ stu_apply.js
@@ -1,37 +1,58 @@
-if(k.request.method == 'POST'){
-	var stu_id = k.request.stu_id;
-	var id = k.request.id;
-	var status = k.request.status;
-	var back = k.session.get("key");
+//学生报名实训，确认实训的api
+
+if (k.request.method == 'POST') {
+    var stu_id = k.request.stu_id;
+    var id = k.request.id;
+    var status = k.request.status;
+    var back = k.session.get("key");
     var studentId = back.studentId;
-    var submitTime = (new Date()).getTime();
-    if(!studentId || studentId != stu_id){
-    	var obj = {code:"100", msg:"登陆已过期，请重新登陆"};
+    if (!studentId || studentId != stu_id) {
+        var obj = {
+            code: "100",
+            msg: "登陆已过期，请重新登陆"
+        };
         k.response.json(obj);
-    }
-    else{
+    } else {
         var table = k.database.getTable("stu_apply");
-        var obj = {stu_id: stu_id, id: id, apply_status: status,submit_time:submitTime};
-        table.add(obj);  
-        var obj = {code:"200", msg:"处理成功"};
+        var obj = {
+            stu_id: stu_id,
+            id: id,
+            apply_status: status
+        };
+        table.add(obj);
+        var obj = {
+            code: "200",
+            msg: "处理成功"
+        };
         k.response.json(obj);
     }
-}
-if(k.request.method == 'PUT'){
+} else if (k.request.method == 'PUT') {
     var apply_id = k.request.apply_id;
     var apply_status = k.request.apply_status;
-	var back = k.session.get("key");
+    var back = k.session.get("key");
     var studentId = back.studentId;
-    if(!studentId || studentId != stu_id){
-    	var obj = {code:"100", msg:"登陆已过期，请重新登陆"};
+    if (!studentId || studentId != stu_id) {
+        var obj = {
+            code: "100",
+            msg: "登陆已过期，请重新登陆"
+        };
         k.response.json(obj);
-    }
-    else{
+    } else {
         var table = k.database.getTable("stu_apply");
-        var stu_apply = table.find("_id",apply_id);
+        var stu_apply = table.find("_id", apply_id);
         stu_apply.apply_status = apply_status;
         table.update(stu_apply);
-        var obj = {code:"200", msg:"处理成功"};
+        if (apply_status == 4) {
+            var achievementTable = k.database.getTable("stu_achievement");
+            var achievementObj = {
+                apply_id: apply_id,
+            };
+            achievementTable.add(achievementObj);
+        }
+        var obj = {
+            code: "200",
+            msg: "处理成功"
+        };
         k.response.json(obj);
     }
 }
