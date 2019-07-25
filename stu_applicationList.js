27313b12f3ba0e62ed0ff9081c66d45956520488
luoxiangyuan <1269091926@qq.com>
2019-07-25 09:29:12 +0800

    add stu_applicationList

---
+++ stu_applicationList.js
@@ -0,0 +1,40 @@
+// sample code..
+//var obj = {};
+//obj.name = "myname";
+//obj.fieldtwo = "value of field two";
+//k.response.setHeader("Access-Control-Allow-Origin", "*");
+//k.response.json(obj);
+var obj = {};
+code = "100";
+msg = "处理失败";
+data = null;
+if (k.request.method == "GET") {
+  var back = k.session.get("key");
+  if (!back) {
+    msg = "登陆过期，请重新登录";
+  } else {
+    var stuId = back.studentId;
+    if (!stuId) {
+      msg = "登陆过期，请重新登录";
+    } else {
+      var status = k.request.status;
+      var page = k.request.page;
+      var stuApply = k.database.stu_apply
+        .query("stu_id ==" + stuId && "apply_status ==" + status)
+        .orderByDescending("submit_time")
+        .skip((page - 1) * 10)
+        .take(10);
+      if (stuApply) {
+        data = stuApply;
+        code = "200";
+        msg = "处理成功";
+      } else {
+        msg = "该学生没有申请记录";
+      }
+    }
+  }
+  obj.code = code;
+  obj.msg = msg;
+  obj.data = data;
+  k.response.json(obj);
+}
